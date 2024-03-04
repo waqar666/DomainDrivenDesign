7e564987eb4d73b3e864be98670283d3692ecfa9
Waqar Qureshi <waqar666@gmail.com>
2024-03-04 12:30:32 +0530

    Add files via upload

---
+++ MarketPlace/Api/ClassifiedAdsApplicationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marketplace.Domain;
+using Marketplace.Framework;
+using Marketplace.Contracts;
+
+namespace Marketplace.Api
+{
+    public class ClassifiedAdsApplicationService : IApplicationService
+    {
+        IClassifiedAdsRepository _repository; ICurrencyLookup _currencyLookup; 
+        public ClassifiedAdsApplicationService(IClassifiedAdsRepository repository, ICurrencyLookup currencyLookup)
+        {
+            _repository = repository; _currencyLookup = currencyLookup;
+        }
+       public async Task Handle(object command)
+        {
+            switch(command)
+            {
+                case ClassifiedAds.V1.Create cmd:
+                    if (await _repository.Exists(cmd.Id.ToString()))
+                        throw new InvalidOperationException($"Entity with id {cmd.Id} already exists");
+                    var classifiedAd = new ClassifiedAd(
+                        new ClassifiedAdId(cmd.Id),
+                        new UserId(cmd.OwnerId));
+                    await _repository.Save(classifiedAd);
+                    break;
+            }
+        }
+    }
+}
---
+++ MarketPlace/Api/ClassifiedAdsCommandsApi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marketplace.Api
+{
+    public class ClassifiedAdsCommandsApi
+    {
+        ClassifiedAdsApplicationService _classifiedAdsApplicationService;
+        public ClassifiedAdsCommandsApi(ClassifiedAdsApplicationService classifiedAdsApplicationService)
+        {
+            _classifiedAdsApplicationService = classifiedAdsApplicationService;
+        }
+        [HttpPost]
+        public Task<IActionResult> post(Contracts.ClassifiedAds.V1.Create request)
+        {
+
+        }
+    }
+}
